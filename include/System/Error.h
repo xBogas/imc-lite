84d3665fbeb5bc0ecaae0efc0f9b55f47986e2fc
xBogas <119903183+xBogas@users.noreply.github.com>
2024-03-02 22:28:33 +0000

    include/System: Add folder for platform abstractions.
    Add Time.h header for clock manipulations.
    
    Add Error.h for error handling.

---
+++ include/System/Error.h
@@ -0,0 +1,7 @@
+#ifndef SYSTEM_ERROR_H_INCLUDED_
+#define SYSTEM_ERROR_H_INCLUDED_
+
+void error(const char* str = nullptr);
+
+
+#endif // SYSTEM_SYSTEM_H_INCLUDED_
---
+++ include/System/Time.h
@@ -0,0 +1,68 @@
+#ifndef SYSTEM_TIME_H_INCLUDED_
+#define SYSTEM_TIME_H_INCLUDED_
+
+#include <stdint.h>
+
+class Clock {
+public:
+	// Get instance of Clock
+	static Clock* getInstance(void);
+
+	// Set current time
+	// @param epoch number of seconds since UNIX epoch
+	static void setEpoch(float epoch);
+
+	// Print current time in format "ddd yyyy-mm-dd hh:mm:ss"
+	static void printTime(void);
+
+	// Get number of seconds since UNIX epoch
+	static float getEpoch(void);
+
+	// Get number of milliseconds since system start
+	static uint32_t getMs(void);
+
+	// Busy wait for ms milliseconds
+	static void busyWait(uint32_t ms);
+
+	//! Non-copyable
+	Clock(Clock&) = delete;
+	Clock& operator=(Clock&) = delete;
+
+private:
+	Clock(void);
+
+	~Clock(void);
+
+	// Update internal date
+	void updateDate(void);
+
+	// Update internal time
+	void updateTime(void);
+
+	// Get current time
+	struct tm getTime(void);
+
+	// Save date to internal variables
+	void saveDate(uint8_t year, uint8_t month, uint8_t day, uint8_t weekday);
+
+	// Save time to internal variables
+	void saveTime(uint8_t hours, uint8_t minutes, uint8_t seconds);
+
+	struct Date {
+		uint8_t year;
+		uint8_t month;
+		uint8_t day;
+		uint8_t weekday;
+	};
+
+	struct Time {
+		uint8_t hours;
+		uint8_t minutes;
+		uint8_t seconds;
+	};
+
+	struct Date _date;
+	struct Time _time;
+};
+
+#endif // CORE_TIME_H_INCLUDED_
