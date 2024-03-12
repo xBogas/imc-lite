552193d12144e9916316b3120089ce1cec1960ca
xBogas <119903183+xBogas@users.noreply.github.com>
2024-03-12 15:58:37 +0000

    src/System/Error: Replace clock delay with CPU cycles.
    This change allows error to blink led even in irq above Systick

--- src/System/Error.cpp
+++ src/System/Error.cpp
@@ -5,6 +5,12 @@
 #include "System/Error.h"
 #include "System/Time.h"
 
+static void cycles_wait(uint32_t cycles)
+{
+	uint32_t start = DWT->CYCCNT;
+	while (DWT->CYCCNT - start < cycles)
+		;
+}
 
 // Print error message
 // Start blinking red LED
@@ -13,12 +19,17 @@ void error(const char* str)
 	if (str != nullptr)
 		Debug(str);
 
-	// TODO: Replace with proper error handling
+	// Enable DWT
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+
+	// Enable the CPU cycle counter
+	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
 	digitalWrite(LED_GREEN, LOW);
 	while (1) {
 		digitalWrite(LED_RED, LOW);
-		Clock::busyWait(500);
+		cycles_wait(SystemCoreClock / 2);
 		digitalWrite(LED_RED, HIGH);
-		Clock::busyWait(500);
+		cycles_wait(SystemCoreClock / 2);
 	}
 }
