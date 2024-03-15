cebd605c66e8329a7237be7373a37ffbec8df893
xBogas <119903183+xBogas@users.noreply.github.com>
2024-03-15 19:21:13 +0000

    Add include folder for Core headers

---
+++ include/Core.h
@@ -0,0 +1,11 @@
+#ifndef CORE_H_INCLUDED_
+#define CORE_H_INCLUDED_
+
+#include "Core/Consumers.h"
+#include "Core/Interface.h"
+#include "Core/Manager.h"
+#include "Core/Task.h"
+#include "Core/Thread.h"
+#include "Core/ThreadQueue.h"
+
+#endif
