d74c4afb398500277446f8b208b66009d609477b
xBogas <119903183+xBogas@users.noreply.github.com>
2024-03-16 15:39:45 +0000

    include/System/Timers: Fix header guards and remove some testing functions.

--- include/System/Timers.h
+++ include/System/Timers.h
@@ -1,5 +1,5 @@
-#ifndef CORE_TIMERS_H_INCLUDED_
-#define CORE_TIMERS_H_INCLUDED_
+#ifndef SYSTEM_TIMERS_H_INCLUDED_
+#define SYSTEM_TIMERS_H_INCLUDED_
 
 #include <cstdint>
 
@@ -9,8 +9,4 @@ void init_timer_pool(void);
 // Add a callback to the timer pool
 void add_callback(void (*callback)(void*), void* args, uint32_t interval, const char* str);
 
-void print_arr(void);
-
-void test_heap(void);
-
-#endif // CORE_TIMERS_H_INCLUDED_
+#endif // SYSTEM_TIMERS_H_INCLUDED_
