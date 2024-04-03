782126216cc96063770e915edb35de44670409b6
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-03 01:24:38 +0100

    include/System/Error: Update error message to allow variable number of arguments and add debug function.

--- include/System/Error.h
+++ include/System/Error.h
@@ -1,7 +1,20 @@
 #ifndef SYSTEM_ERROR_H_INCLUDED_
 #define SYSTEM_ERROR_H_INCLUDED_
 
-void error(const char* str = nullptr);
+#include "Defines.h"
 
+_BEGIN_STD_C
+
+/// @brief Print an error message and exit the program
+/// @param format The format string
+/// @param ... The arguments to the format string
+void error(const char* format, ...);
+
+/// @brief Print a debug message
+/// @param format The format string
+/// @param ... The arguments to the format string
+void debug(const char* format, ...);
+
+_END_STD_C
 
 #endif // SYSTEM_SYSTEM_H_INCLUDED_
