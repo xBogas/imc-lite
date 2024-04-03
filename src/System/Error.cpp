a88a9c2358a6b327bd207fc8de531339f42dfc20
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-03 01:31:31 +0100

    src/System/Error: Update error implementation to be force uart transmission blink LEDs after. Set debug to force uart transmission.
    Force both functions to use C compilation/linkage

--- src/System/Error.cpp
+++ src/System/Error.cpp
@@ -5,6 +5,11 @@
 #include "System/Error.h"
 #include "System/Time.h"
 
+_BEGIN_STD_C
+
+static const char* eof = "\r\n";
+static size_t eof_size = strlen(eof);
+
 static void cycles_wait(uint32_t cycles)
 {
 	uint32_t start = DWT->CYCCNT;
@@ -14,10 +19,16 @@ static void cycles_wait(uint32_t cycles)
 
 // Print error message
 // Start blinking red LED
-void error(const char* str)
+static void fmt_error(const char* str)
 {
-	if (str != nullptr)
-		Debug(str);
+	if (str != nullptr) {
+		UART_HandleTypeDef* handle = Serial.getHandle();
+		HAL_UART_AbortTransmit(handle);
+
+		HAL_UART_Transmit(handle, (const uint8_t*)str, strlen(str),
+						  HAL_MAX_DELAY);
+		HAL_UART_Transmit(handle, (const uint8_t*)eof, eof_size, HAL_MAX_DELAY);
+	}
 
 	// Enable DWT
 	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
@@ -33,3 +44,46 @@ void error(const char* str)
 		cycles_wait(SystemCoreClock / 2);
 	}
 }
+
+void error(const char* str, ...)
+{
+	__disable_irq();
+
+	if (str == NULL)
+		fmt_error(NULL);
+
+	char buffer[256];
+	va_list args;
+	va_start(args, str);
+	vsnprintf(buffer, 256, str, args);
+	va_end(args);
+
+	fmt_error(buffer);
+}
+
+void debug(const char* str, ...)
+{
+	if (str == NULL)
+		return;
+
+	char buffer[256];
+	va_list args;
+	va_start(args, str);
+	vsnprintf(buffer, 256, str, args);
+	va_end(args);
+
+	UART_HandleTypeDef* handle = Serial.getHandle();
+	HAL_UART_AbortTransmit(handle);
+
+	HAL_UART_Transmit(handle, (const uint8_t*)buffer, strlen(buffer),
+					  HAL_MAX_DELAY);
+	HAL_UART_Transmit(handle, (const uint8_t*)eof, eof_size, HAL_MAX_DELAY);
+}
+
+
+void HardFault_Handler(void)
+{
+	error("HardFault_Handler");
+}
+
+_END_STD_C
