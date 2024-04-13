14324ffce7922a05081c84fab06588e70dc5ce9f
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-13 14:26:45 +0100

    include/Core: Add copyright notice to Core.h

--- include/Core.h
+++ include/Core.h
@@ -1,11 +1,18 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
 #ifndef CORE_H_INCLUDED_
 #define CORE_H_INCLUDED_
 
+#include "Core/AbstractTask.h"
 #include "Core/Consumers.h"
 #include "Core/Interface.h"
 #include "Core/Manager.h"
 #include "Core/Task.h"
-#include "Core/Thread.h"
-#include "Core/ThreadQueue.h"
 
 #endif
