e08c3557c96794780979938db83ba2068fce32f5
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-13 14:26:32 +0100

    include/System: Add System.h header file

---
+++ include/System.h
@@ -0,0 +1,18 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
+#ifndef SYSTEM_H_INCLUDED_
+#define SYSTEM_H_INCLUDED_
+
+#include "System/Error.h"
+#include "System/Flash.h"
+#include "System/Terminal.h"
+#include "System/Time.h"
+#include "System/Timers.h"
+
+#endif
