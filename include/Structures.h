3e97173f4761e6c7674947ea9b9fd72e66de4c6a
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-13 14:09:06 +0100

    include/Strutures: Add header file with all strutures implementations

---
+++ include/Structures.h
@@ -0,0 +1,16 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
+#ifndef STRUCTURES_H_INCLUDED_
+#define STRUCTURES_H_INCLUDED_
+
+#include "Structures/Heap.h"
+#include "Structures/List.h"
+#include "Structures/Queue.h"
+
+#endif
