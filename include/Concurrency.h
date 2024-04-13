270f4423c5deae21404cc54bb9d3e9124546c11b
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-13 14:27:56 +0100

    include: Add Concurrency.h header

---
+++ include/Concurrency.h
@@ -0,0 +1,16 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
+#ifndef CONCURRENCY_H_INCLUDED_
+#define CONCURRENCY_H_INCLUDED_
+
+#include "Concurrency/Mutex.h"
+#include "Concurrency/Scheduler.h"
+#include "Concurrency/Thread.h"
+
+#endif
