e0202842057e275e9519619f7040b27be6476c11
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-13 14:04:04 +0100

    include/IMC: Add imc related headers

--- include/IMC.h
+++ include/IMC.h
@@ -1,10 +1,26 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
 #ifndef IMC_H_INCLUDED_
 #define IMC_H_INCLUDED_
 
+#include "IMC/Bitfields.h"
+#include "IMC/Blob.h"
 #include "IMC/Constants.h"
 #include "IMC/Definitions.h"
+#include "IMC/Enumerations.h"
+#include "IMC/Header.h"
+#include "IMC/InlineMessage.h"
 #include "IMC/Macros.h"
+#include "IMC/Message.h"
+#include "IMC/MessageList.h"
 #include "IMC/Parsers.h"
 #include "IMC/Serialization.h"
+#include "IMC/SuperTypes.h"
 
 #endif
