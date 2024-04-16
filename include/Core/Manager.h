00e8179f214cf2af0e7e39fdb23bd81a06d5b959
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-17 00:54:31 +0100

    Core/Manager: Reimplement class to handle parameter write/save to flash.

--- include/Core/Manager.h
+++ include/Core/Manager.h
@@ -10,64 +10,212 @@
 #define CORE_MANAGER_H_INCLUDED_
 
 #include "Concurrency/Thread.h"
-
 #include "Core/Consumers.h"
+#include "Core/Task.h"
+#include "Structures/List.h"
+#include "System/Flash.h"
 
-// class TaskManager {
-// public:
-// 	TaskManager(void)
-// 	{ }
+#include <map>
 
-// 	~TaskManager(void)
-// 	{ }
+// Load memory Parameters
+class TaskManager {
+	struct MemParam {
+		u32 len;  // Length of the task parameters
+		u32 addr; // Address of the task parameters
+	};
 
-// 	// Register thread to task manager
-// 	void registerTask(thread* task)
-// 	{
-// 		thr_queue.push(task);
-// 	}
+	typedef std::map<std::string, MemParam> MemParamMap;
 
-// 	// Start task scheduler
-// 	void start(void);
+public:
+	TaskManager(void)
+	  : flash_addr(FLASH_USER_ADDR),
+		flash_base(FLASH_USER_ADDR),
+		tsk_list(NULL)
+	{
+		tsk_list = list_create();
+		ASSERT_ERR(tsk_list != NULL, "Failed to create task list!");
+	}
 
-// 	// Check if task manager is running
-// 	bool isRunning(void)
-// 	{
-// 		return running;
-// 	}
+	~TaskManager(void)
+	{ }
 
-// 	// Pop thread from queue
-// 	thread* popThread(void)
-// 	{
-// 		return thr_queue.pop();
-// 	}
+	/// @brief Register a task to the task manager
+	/// @param tsk Task to register
+	void registerTask(Task* tsk)
+	{
+		list_append(tsk_list, tsk);
+		debug("Task %s registered", tsk->getName());
+	}
 
-// 	// Push thread to queue
-// 	void pushThread(thread* thr)
-// 	{
-// 		thr_queue.push(thr);
-// 	}
+	/// @brief Setup tasks from flash memory
+	void setupTasks(void)
+	{
+		// Key -> [task label, MemParam]
+		MemParamMap mem_params;
+		u32 _addr = flash_base;
 
-// 	void setBackgroundTask(thread* thr)
-// 	{
-// 		background = thr;
-// 	}
+		do {
+			_addr = getMemParams(mem_params, _addr);
+		} while (_addr != 0);
 
-// 	uint32_t getQueueSize(void)
-// 	{
-// 		return thr_queue.size();
-// 	}
+		Task* tsk = (Task*)list_pop(tsk_list);
+		while (tsk != NULL) {
 
-// private:
-// 	thread* background;
-// 	// thread queue
-// 	// ThreadQueue thr_queue;
-// 	// os Status
-// 	bool running = false;
-// };
+			// debug("Setting up task %s", tsk->getName());
+			std::string label = tsk->getName();
+			auto it = mem_params.find(label);
+			if (it != mem_params.end())
+				loadTaskParams(tsk, &it->second);
 
-// void dispatch_thread(void*);
+			tsk = (Task*)list_pop(tsk_list);
+		}
+	}
 
-// extern class TaskManager Manager;
+	/**
+	 * [11][len_1][task name][len_2][task parameters][checksum][11]...[0xFF]
+	 * 11 - Start symbol
+	 * len - Length of the task name or parameters
+	 * task name - Name of the task
+	 * len - Length of the task parameters
+	 * task parameters - Parameters of the task
+	 * checksum - CRC16 checksum of every byte from start to end of the task
+	 * parameters
+	 *
+	 * 0xFF - End symbol (make shure is not 11)
+	 */
+
+	/// @brief Save all task parameters to flash memory
+	void saveAllParams(void)
+	{
+		// flash_erase_section(FLASH_USER_SECT);
+		u32 flash_addr = flash_base;
+
+		lst_node* node = tsk_list->head;
+		while (node != NULL) {
+
+			Task* tsk = (Task*)node->elem;
+			flash_addr += saveTaskParams(tsk, flash_addr);
+			node = node->next;
+		}
+
+		u8 end = 0xFF; // Make shure symbol is not 11
+		flash_write(flash_addr, &end, 1);
+	}
+
+private:
+	/// @brief Get memory parameters from flash memory
+	/// @param params Task parameters map
+	/// @param _ptr Flash memory address pointer
+	/// @return Next flash memory address pointer
+	u32 getMemParams(MemParamMap& params, u32 _ptr)
+	{
+		// debug("Reading memory parameters at %d", _ptr);
+		u32 start = _ptr;
+		u8 symbol;
+		flash_read(_ptr, &symbol, sizeof(symbol));
+		if (symbol != 11) {
+			// debug("Invalid start symbol: %d", symbol);
+			return 0;
+		}
+		_ptr += sizeof(symbol);
+
+		// Read task name length
+		u16 len = 0;
+		flash_read(_ptr, &len, sizeof(len));
+		if (len > 64) {
+			// debug("Task name too long: %d", len);
+			return 0;
+		}
+		_ptr += sizeof(len);
+		// debug("Task name length: %d", len);
+
+		// Read task name
+		std::string tsk_label;
+		for (size_t i = 0; i < len; i++) {
+			char c;
+			flash_read(_ptr + i, &c, sizeof(c));
+			tsk_label.push_back(c);
+		}
+		// debug("Task name: %s", tsk_label.c_str());
+		_ptr += len;
+
+		// Read parameters length
+		flash_read(_ptr, &len, sizeof(len));
+		// debug("Task parameters length: %d", len);
+		_ptr += sizeof(len);
+		u32 param_start = _ptr;
+
+		// Move pointer to end of task parameters
+		_ptr += len;
+		u16 checksum;
+		flash_read(_ptr, &checksum, sizeof(checksum));
+
+		// Checksum validation
+		u16 crc = IMC::compute_CRC16((u8*)start, _ptr - start);
+		if (crc != checksum) {
+			// debug("Param integrity failed: %d != %d", crc, checksum);
+			return 0;
+		}
+		_ptr += sizeof(checksum);
+
+		// Add params to map
+		MemParam& p = params[tsk_label];
+		p.len = len;
+		p.addr = param_start;
+
+		return _ptr;
+	}
+
+	/// @brief Load task parameters from flash memory
+	/// @param tsk Task to load
+	/// @param p Memory parameters
+	/// @return True if successful
+	bool loadTaskParams(Task* tsk, MemParam* p)
+	{
+		u8 bfr[1024];
+		flash_read(p->addr, bfr, p->len);
+		return tsk->loadParams(bfr, p->len);
+	}
+
+	/// @brief Save task parameters to flash memory
+	/// @param tsk Task to save
+	/// @param _ptr Flash memory address pointer
+	/// @return Number of bytes written
+	u32 saveTaskParams(Task* tsk, u32 _ptr)
+	{
+		// debug("Saving task %s at %d", tsk->getName(), _ptr);
+		u32 start = _ptr;
+		u8 symbol = 11;
+		_ptr = flash_write(_ptr, &symbol, 1);
+
+		const char* label = tsk->getName();
+		u16 len = strlen(label);
+
+		_ptr = flash_write(_ptr, &len, sizeof(len));
+		_ptr = flash_write(_ptr, label, len);
+
+		u8 bfr[1024];
+		len = tsk->saveParams(bfr, 1024);
+
+		// debug("Task parameters length: %d", len);
+		_ptr = flash_write(_ptr, &len, sizeof(len));
+		_ptr = flash_write(_ptr, bfr, len);
+
+		u16 crc = IMC::compute_CRC16((u8*)start, _ptr - start);
+		_ptr = flash_write(_ptr, &crc, sizeof(crc));
+
+		// debug("Task crc %d saved", crc);
+		return _ptr - start;
+	}
+
+	// Current Flash memory address
+	u32 flash_addr;
+	// Base Flash memory address
+	const u32 flash_base;
+	// Task list
+	struct list* tsk_list;
+};
+
+extern TaskManager Manager;
 
 #endif // CORE_MANAGER_H_INCLUDED_
