d9fdd354241fc989482e4d1a24d7576ef362f45f
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-17 19:30:12 +0100

    include/Structures/Queue: Add/Update comments

--- include/Structures/Queue.h
+++ include/Structures/Queue.h
@@ -22,25 +22,33 @@ enum {
 	QUEUE_ERR_FULL = 1,
 };
 
-// First In Last Out Queue
+// First In First Out Queue
 struct queue {
 	queue_size_t size;
 	queue_size_t capacity;
 	void** arr;
 };
 
-//TODO Add comments
-
+/// @brief Initialize a queue
+/// @param queue Queue to be initialized
+/// @param capacity Capacity of the queue
 void queue_init(struct queue* queue, queue_size_t capacity);
 
+/// @brief Create a new queue
+/// @param capacity Capacity of the queue
+/// @return The new queue
 struct queue* queue_create(queue_size_t capacity);
 
+/// @brief Destroy a queue
 void queue_destroy(struct queue* list);
 
+/// @brief Push an element to the queue
 queue_err_t queue_push(struct queue* list, void* data);
 
+/// @brief Pop an element from the queue
 void* queue_pop(struct queue* list);
 
+/// @brief Get the size of the queue
 static queue_size_t queue_size(struct queue* list)
 {
 	return list->size;
