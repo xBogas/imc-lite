0a195e1c9e7b3fce00450013bbfd60ee8b218d48
xBogas <119903183+xBogas@users.noreply.github.com>
2024-04-18 19:38:56 +0100

    src/Concurrency/MessageQueue: Signal cond var on successful queue push. Add memory assertion.

--- src/Concurrency/MessageQueue.c
+++ src/Concurrency/MessageQueue.c
@@ -21,6 +21,7 @@ void mq_init(struct mq* mq, u32 size)
 struct mq* mq_create(u32 size)
 {
 	struct mq* mq = (struct mq*)malloc(sizeof(struct mq));
+	ASSERT_ERR(mq != NULL, "Failed to allocate memory for message queue");
 	mq_init(mq, size);
 
 	return mq;
@@ -45,13 +46,19 @@ void mq_push(struct mq* mq, void* data)
 
 		err = queue_push(mq->msg_q, data);
 	}
+
+	cond_signal_all(mq->cv);
 }
 
 mq_err_t mq_try_push(struct mq* mq, void* data)
 {
 	ASSERT_ERR(mq != NULL, "Message queue is NULL");
 
-	return (mq_err_t)queue_push(mq->msg_q, data);
+	queue_err_t err = queue_push(mq->msg_q, data);
+	if (err == QUEUE_ERR_NONE)
+		cond_signal_all(mq->cv);
+
+	return err;
 }
 
 void* mq_pop(struct mq* mq)
@@ -70,6 +77,9 @@ void* mq_pop(struct mq* mq)
 void* mq_pop_timeout(struct mq* mq, u32 ms)
 {
 	ASSERT_ERR(mq != NULL, "Message queue is NULL");
+
+	if (ms == 0)
+		return mq_pop(mq);
 
 	void* data = queue_pop(mq->msg_q);
 	while (data == NULL) {
