2255d5599f6154b9e6b6529a6fa0dab47e4aa534
xBogas <119903183+xBogas@users.noreply.github.com>
2024-05-03 23:32:57 +0100

    include/IMC/Header: Remove float to double convertion functions and definitions.

--- include/IMC/Header.h
+++ include/IMC/Header.h
@@ -15,55 +15,8 @@
 // IMC headers.
 #include "IMC/Constants.h"
 
-#define SIG_BITS_FP64 0x8000000000000000
-#define EXP_BITS_FP64 0x7FF0000000000000 // 0x7F8 ?
-#define FRA_BITS_FP64 0x000FFFFFE0000000
-
-#define SIG_BITS_FP32 0x80000000
-#define EXP_BITS_FP32 0x7F800000
-#define FRA_BITS_FP32 0x007FFFFF
-
 typedef double fp64_t;
 typedef float fp32_t;
-
-union fconv_t {
-	float data;
-	uint32_t bits;
-};
-
-static float to_float(const uint64_t& var)
-{
-	uint32_t sig = (var & SIG_BITS_FP64) >> 32;
-	uint32_t frac = (var & FRA_BITS_FP64) >> 29;
-
-	int16_t _exp = ((var & EXP_BITS_FP64) >> 52) - 1023;
-
-	uint8_t exp;
-	if (_exp < -127) {
-		exp = 0;
-		frac = 0;
-	} else if (_exp >= 128) {
-		exp = 255;
-		frac = 0;
-	} else
-		exp = _exp + 127;
-
-	fconv_t u = {.bits = (sig | (exp << 23) | frac)};
-	return u.data;
-}
-
-static uint64_t to_fp64(const uint32_t var)
-{
-	uint64_t sig = (var & SIG_BITS_FP32);
-	sig = sig << 32;
-
-	uint8_t exp = (var & EXP_BITS_FP32) >> 23;
-	uint64_t _exp = ((exp - 127) + 1023);
-	_exp = _exp << 52;
-
-	uint64_t frac = (var & FRA_BITS_FP32) << 29;
-	return (sig | _exp | frac);
-}
 
 namespace IMC {
 /// Header format.
