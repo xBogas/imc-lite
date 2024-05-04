516894c7da085186005c4d2c67ef3093f68affac
xBogas <119903183+xBogas@users.noreply.github.com>
2024-05-04 13:36:29 +0100

    Drivers: Add header for drivers includes.

---
+++ include/Drivers.h
@@ -0,0 +1,14 @@
+// ****************************************************************
+// Copyright 2024 Universidade do Porto - Faculdade de Engenharia *
+// Laboratório de Sistemas e Tecnologia Subaquática (LSTS)        *
+// Departamento de Engenharia Electrotécnica e de Computadores    *
+// ****************************************************************
+// Author: João Bogas                                             *
+// ****************************************************************
+
+#ifndef DRIVERS_H_INCLUDED_
+#define DRIVERS_H_INCLUDED_
+
+#include "Drivers/ethernet.h"
+
+#endif
